3276808d489e1b58b81cbce635a8cb0ff936a5f8
stormzee <soppongaddo@gmail.com>
2022-02-01 12:36:33 +0000

    added exception handling

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from typing import Optional
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, HTTPException, Request
+from fastapi.exception_handlers import http_exception_handler, request_validation_exception_handler
+from starlette.exceptions import HTTPException as starletteHTTPException
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import PlainTextResponse, JSONResponse
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 import joblib
 import numpy as np
@@ -8,7 +13,31 @@ warnings.filterwarnings("ignore")
 
 app = FastAPI()
 
+# for you to perform custom exception handling, 
+# use the starlette or fastapi exception_handler and plaintextresponses
+# to override the built-in fastapi exception handler
 
+@app.exception_handler(starletteHTTPException)
+async def custom_exception_handler(request, exc):
+    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    return PlainTextResponse(str(exc), status_code=422)
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request:Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code= status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content = jsonable_encoder({
+            "detail":str(exc.errors),
+            "body": exc.body
+            
+        }),
+    )
+    
+    
 class UserInput(BaseModel):
     deposits : int
     withdrawals : int 
@@ -30,8 +59,13 @@ def load_utils():
 @app.post("/predict/", status_code = status.HTTP_200_OK)
 def model_prediction(input_data:UserInput):
     model =  load_utils()
-    try:
-        final_prediction = model.predict(np.array(
+    
+    
+    if not input_data:
+        raise HTTPException(status_code=500, detail="Input not acceptable, please provide the correct input")
+
+
+    final_prediction = model.predict(np.array(
             [input_data.deposits,
             input_data.withdrawals,
             input_data.savings,
@@ -40,15 +74,17 @@ def model_prediction(input_data:UserInput):
             input_data.employed,
             input_data.available_pension_funds,
             input_data.business_profit]).reshape(1,-1))[0]
-        explanations = "financially resilient"
+    
         
-        if not final_prediction :
-            explanations = "You're not Financially Resilient"
+    explanations = "financially resilient"
+        
+    if not final_prediction :
+        explanations = "You're not Financially Resilient"
             
-        response = {"final_prediction" : int(final_prediction), "Interpretation":explanations}
-   
-    except ValueError:
-        response = {"message":"No results for your input","status":status.HTTP_406_NOT_ACCEPTABLE, "Error":type(error)}
+    response = {"final_prediction" : int(final_prediction), "Interpretation":explanations}
+   # response = {"message":"No results for your input","status":status.HTTP_406_NOT_ACCEPTABLE, "Error":type(error)}
+    
+    
     return response
 
 
